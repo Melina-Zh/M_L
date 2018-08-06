6528618419bc015fe52c19da44872cc85083414b
Melina-Zh <zhynzm_85@126.com>
2018-08-06 22:54:52 +0800

    k-

--- KNNDemo.py
+++ KNNDemo.py
@@ -5,8 +5,14 @@ Created on Mon Aug  6 21:08:26 2018
 
 @author: Melina
 """
+import matplotlib.pyplot as plt
 import KNN
 group,labels=KNN.createDataSet()
 #print(group)
 #print(labels)
-print(KNN.classify0([0,0],group,labels,3))
+
+print(KNN.classify0([0,3],group,labels,3))
+
+plt.scatter(group[0:4,0],group[0:4,1],s=50)
+plt.scatter(0,3,s=50)
+plt.show()
